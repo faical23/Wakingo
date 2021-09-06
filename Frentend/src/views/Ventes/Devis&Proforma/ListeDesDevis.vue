9fc3d27d92c529f4b2197142f1f6fefd8df8b228
Faical Bahsis <65980103+faical23@users.noreply.github.com>
2021-09-06 23:28:45 +0100

    add

---
+++ Frentend/src/views/Ventes/Devis&Proforma/ListeDesDevis.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="HomePage">
+      <SideBar/>
+      <div class="Content">
+          this is lise des ventes
+          
+      </div>
+  </div>
+</template>
+
+<script>
+  import SideBar from '../../components/SideBar/Index.vue'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      SideBar,
+      
+    },
+  }
+</script>
